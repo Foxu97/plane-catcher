f228cfea8a2c09a9dbded89a2367e4f7b0b73b16
Kornel Fox/PC <kolo1908@gmail.com>
2019-12-05 17:39:29 +0100

    Changed getCurrentPosition to watchPosition, added user marker, added askPermission button.

--- mobile/screens/MapScreen.js
+++ mobile/screens/MapScreen.js
@@ -1,70 +1,81 @@
 import React, { useState, useEffect } from 'react';
 import { HeaderButtons, Item } from 'react-navigation-header-buttons';
 import MapView from 'react-native-maps';
-import * as Location from 'expo-location';
 import * as Permissions from 'expo-permissions';
 import { View, Text, Button, StyleSheet, Dimensions, Alert, Platform } from 'react-native';
 
 import Colors from '../constants/Colors';
+import manMarker from '../assets/standing-up-man-.png';
 import CustomHeaderButton from '../components/CustomHeaderButton';
 
 const MapScreen = props => {
     const [userLocation, setUserLocation] = useState();
+    const [hasLocationPermission, setHasLocationPermission] = useState(false);
 
-    const planes = [
-        {title: "uno", coordinates: {
-            latitude: 50.300421, longitude: 18.681935
-        }},
-        {title: "secundo", coordinates: {
-            latitude: 50.543212, longitude: 18.770099
-        }}
-    ] 
+    const setRegion = () => ({
+        latitude: userLocation.latitude,
+        longitude: userLocation.longitude,
+        latitudeDelta: 0.0922,
+        longitudeDelta: 0.0421
+    });
 
-
-    useEffect(() => {
-        const verifyPermissions = async () => {
+    const verifyPermissions = async () => {
+        try {
             const result = await Permissions.askAsync(Permissions.LOCATION);
             if (result.status !== 'granted') {
-                Alert.alert(
-                    'Insufficient permissions!',
-                    'You need to grant location permissions to use this app.',
-                    [{ text: 'Okay' }]
-                );
-                return false;
+                Alert.alert('Insufficient permissions!', [{ text: 'OK!' }]);
+                //console.log("No permission for LOCATION granted");
+            } else {
+                setHasLocationPermission(true);
+                //console.log("Permission for LOCATION granted");
+                const watchID = navigator.geolocation.watchPosition(
+                    (position) => {
+                        //console.log("New position: ", position)
+                        setUserLocation({
+                            latitude: position.coords.latitude,
+                            longitude: position.coords.longitude
+                        })
+                    },
+                    (err) => console.log(err),
+                    { enableHighAccuracy: true, timeout: 8000, maximumAge: 10000 }
+                )
+                return watchID;
             }
-            return true;
-        };
-        verifyPermissions().then(result => {
-            if (!result){
-                return;
-            }
-            Location.getCurrentPositionAsync({timeout: 5000}).then((location) => {
-                setUserLocation(location)
-            }).catch(err => {
-                Alert.alert(
-                    'Could not fetch location!',
-                    'Please try again later or pick a location on the map.',
-                    [{ text: 'Okay' }]
-            );
-            });
-        });
-    }, [])
+        } catch (err) {
+            console.log(err)
+        }
+
+    }
+    useEffect(() => {
+        const watchID = verifyPermissions(); //not shure if ok
+        return () => {
+            console.log("Component unmount")
+            navigator.geolocation.clearWatch(watchID);
+        }
+    }, []);
 
 
     return (
         <View style={styles.container}>
             {
-                userLocation ? <MapView region={{
-                    latitude: userLocation.coords.latitude,
-                    longitude: userLocation.coords.longitude,
-                    latitudeDelta: 0.0922,
-                    longitudeDelta: 0.0421
-                    }} style={styles.mapStyle} >{planes.map(plane => (
-                       <MapView.Marker  
-                       coordinate={plane.coordinates}
-                       title={plane.title}/>
-                    ))}</MapView> :
-                    <Text>We need your location permission granted</Text>
+                userLocation && hasLocationPermission ?
+                    <MapView region={{
+                        latitude: userLocation.latitude,
+                        longitude: userLocation.longitude,
+                        latitudeDelta: 0.0922,
+                        longitudeDelta: 0.0421
+                    }} style={styles.mapStyle} >
+
+                        <MapView.Marker
+                            coordinate={setRegion()}
+                            image={manMarker}
+                        />
+                    </MapView>
+                    :
+                    <View>
+                        <Text>We need your location permission granted</Text>
+                        <Button title="Grant GPS Permission" onPress={verifyPermissions}/>
+                    </View>
             }
         </View>
     );
@@ -87,15 +98,15 @@ MapScreen.navigationOptions = navData => {
     return {
         headerTitle: 'Map',
         headerRight: <HeaderButtons HeaderButtonComponent={CustomHeaderButton}>
-            <Item 
-                title="Add Place" 
+            <Item
+                title="Add Place"
                 iconName={Platform.OS === 'android' ? 'md-camera' : 'ios-add'}
                 onPress={() => {
                     navData.navigation.navigate('AR');
                 }}
-                />
-            
-            </HeaderButtons>
+            />
+
+        </HeaderButtons>
     }
 }
 
