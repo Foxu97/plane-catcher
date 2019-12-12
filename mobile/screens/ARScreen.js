3f0aba3f4d479380d29b461fa47392d38efdf322
Kornel Fox/PC <kolo1908@gmail.com>
2019-12-12 20:16:16 +0100

    Getting heading from Location.

--- mobile/screens/ARScreen.js
+++ mobile/screens/ARScreen.js
@@ -1,9 +1,31 @@
 import React, { useState, useEffect } from 'react';
-import { Text, View, TouchableOpacity, StyleSheet } from 'react-native';
+import { Text, View, StyleSheet } from 'react-native';
+import * as Location from 'expo-location';
+import * as Permissions from 'expo-permissions';
 
 import CameraComponent from '../components/CameraComponent';
 
+const samplePlane = {
+    bearingInDegrees: 27.05050,
+    angleBetweenPlaneAndObserverInDegrees: 17.888
+}
+
 const ARScreen = () => {
+    const [hasLocationPermission, setHasLocationPermission] = useState();
+    const [heading, setHeading] = useState();
+
+    const askForLocationPermission = async () => {
+        let { status } = await Permissions.askAsync(Permissions.LOCATION);
+        setHasLocationPermission(status === 'granted');
+
+        Location.watchHeadingAsync((heading) => {
+            console.log(heading)
+        })
+    }
+
+    useEffect(() => {
+        askForLocationPermission();
+    }, [])
 
   return (
     <View style={styles.screen}>
