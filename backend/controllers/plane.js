9c048f10ae8b11e9830718118bc48502d9082ae2
Kornel Fox/PC <kolo1908@gmail.com>
2020-01-17 15:34:55 +0100

    Improved backend code, deleted unused files.

--- backend/controllers/plane.js
+++ backend/controllers/plane.js
@@ -1,31 +1,29 @@
+const API = require('../models/API');
+
 const fetch = require('node-fetch');
 
-var rad = function (x) {
-    return x * Math.PI / 180;
-};
-function getDistance(p1, p2) {
-    var R = 6378137; // Earth’s mean radius in meter
-    var dLat = rad(p2.latitude - p1.latitude);
-    var dLong = rad(p2.longitude - p1.longitude);
-    var a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
-        Math.cos(rad(p1.latitude)) * Math.cos(rad(p2.latitude)) *
+const getDistance = (p1, p2) => {
+    const R = 6378137; // Earth’s mean radius in meter
+    const dLat = toRadians(p2.latitude - p1.latitude);
+    const dLong = toRadians(p2.longitude - p1.longitude);
+    const a = Math.sin(dLat / 2) * Math.sin(dLat / 2) +
+        Math.cos(toRadians(p1.latitude)) * Math.cos(toRadians(p2.latitude)) *
         Math.sin(dLong / 2) * Math.sin(dLong / 2);
-    var c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
-    var d = R * c;
+    const c = 2 * Math.atan2(Math.sqrt(a), Math.sqrt(1 - a));
+    const d = R * c;
     return d;
 };
 
-// Converts from degrees to radians.
-function toRadians(degrees) {
+const toRadians = degrees => {
     return degrees * Math.PI / 180;
   };
    
   // Converts from radians to degrees.
-  function toDegrees(radians) {
+const toDegrees = radians => {
     return radians * 180 / Math.PI;
   }
 
-function calcBearing(userCoords, planeCoords){
+const calcBearing = (userCoords, planeCoords) => {
     const startLat = toRadians(userCoords.latitude);
     const startLng = toRadians(userCoords.longitude);
     const destLat = toRadians(planeCoords.latitude);
@@ -38,9 +36,6 @@ function calcBearing(userCoords, planeCoords){
     brng = toDegrees(brng);
     return (brng + 360) % 360;
 }
-
-// IT WILL BE SPLITTED INTO OTHER FILES
-
 
 exports.getAllPlanesInRange = async (req, res, next) => {
     const userLatitude = req.query.latitude;
@@ -63,7 +58,7 @@ exports.getAllPlanesInRange = async (req, res, next) => {
             });
             if (planesInRangeOfUser) {
                 planesInRangeOfUser.forEach((plane => { 
-                    let mappedARCoords = findMappedCoordinates({ latitude: userLatitude, longitude: userLongitude}, {latitude: plane.latitude, longitude: plane.longitude});
+                    const mappedARCoords = findMappedCoordinates({ latitude: userLatitude, longitude: userLongitude}, {latitude: plane.latitude, longitude: plane.longitude});
                     plane.arLatitude = mappedARCoords.latitude;
                     plane.arLongitude = mappedARCoords.longitude;
                     plane.arPoint = transformPointToAR(plane.arLatitude, plane.arLongitude, userLatitude, userLongitude,  userHeading);
@@ -71,7 +66,6 @@ exports.getAllPlanesInRange = async (req, res, next) => {
                 await asyncForEach(planesInRangeOfUser, async (plane) => {
                     try {
                         const planeInfo = await getPlaneInfo(plane.icao24);
-                        //console.log(planeInfo)
                         if (planeInfo.success !== false){
                             plane.airline = planeInfo[0].airline.icaoCode;
                             plane.aircraft = planeInfo[0].aircraft.icaoCode;
@@ -86,13 +80,10 @@ exports.getAllPlanesInRange = async (req, res, next) => {
 
                 res.status(200).json(planesInRangeOfUser);
             }
-        
-
         }
         else {
             res.status(404).json({message: "No planes in given range!"});
         }
-    
     }
     catch (err) {
         console.log(err);
@@ -133,15 +124,14 @@ const getBoundingBoxCoveringUserPosition = (userCoords, rangeInKm) => {
 }
 
 const fetchPlanesInBoundingBox = async (minBoxPoint, maxBoxPoint) => {
-    return fetch(`https://opensky-network.org/api/states/all?lamin=${minBoxPoint.latitude}&lomin=${minBoxPoint.longitude}&lamax=${maxBoxPoint.latitude}&lomax=${maxBoxPoint.longitude}`).then(
+    return fetch(API.OpenSkyURL + `states/all?lamin=${minBoxPoint.latitude}&lomin=${minBoxPoint.longitude}&lamax=${maxBoxPoint.latitude}&lomax=${maxBoxPoint.longitude}`).then(
         res => {
             return res.json();
         }
     );
 }
 const getPlaneInfo = async(icao24) => {
-    
-    return fetch(`http://aviation-edge.com/v2/public/flights?key=008a4c-e9f4d4&aircraftIcao24=${icao24}`).then(
+    return fetch(API.AviationEdgeURL + `flights?key=${process.env.AVIATION_EDGE_APIKEY}&aircraftIcao24=${icao24}`).then(
         res => {
             return res.json();
         }
@@ -178,26 +168,26 @@ const asyncForEach = async(array, callback) => {
 }
 
 const latLongToMerc = (lat_deg, lon_deg) => {
-    var lon_rad = (lon_deg / 180.0 * Math.PI)
-    var lat_rad = (lat_deg / 180.0 * Math.PI)
-    var sm_a = 6378137.0
-    var xmeters = sm_a * lon_rad
-    var ymeters = sm_a * Math.log((Math.sin(lat_rad) + 1) / Math.cos(lat_rad))
+    const lon_rad = (lon_deg / 180.0 * Math.PI)
+    const lat_rad = (lat_deg / 180.0 * Math.PI)
+    const sm_a = 6378137.0
+    const xmeters = sm_a * lon_rad
+    const ymeters = sm_a * Math.log((Math.sin(lat_rad) + 1) / Math.cos(lat_rad))
     return ({ x: xmeters, y: ymeters });
   }
 
 const transformPointToAR = (objectLat, objectLng, deviceLat, deviceLng, heading) => {
-    var objPoint = latLongToMerc(objectLat, objectLng);
-    var devicePoint = latLongToMerc(deviceLat, deviceLng);
+    const objPoint = latLongToMerc(objectLat, objectLng);
+    const devicePoint = latLongToMerc(deviceLat, deviceLng);
     // latitude(north,south) maps to the z axis in AR
     // longitude(east, west) maps to the x axis in AR
-    var objFinalPosZ = objPoint.y - devicePoint.y;
-    var objFinalPosX = objPoint.x - devicePoint.x;
+    const objFinalPosZ = objPoint.y - devicePoint.y;
+    const objFinalPosX = objPoint.x - devicePoint.x;
     //flip the z, as negative z(is in front of us which is north, pos z is behind(south).
-    let angle = heading;
+    const angle = heading;
     const angleRadian = (angle * Math.PI) / 180; // degree to radian
-    let newRotatedX = objFinalPosX * Math.cos(angleRadian) - objFinalPosZ * Math.sin(angleRadian)
-    let newRotatedZ = objFinalPosZ * Math.cos(angleRadian) + objFinalPosX * Math.sin(angleRadian)
+    const newRotatedX = objFinalPosX * Math.cos(angleRadian) - objFinalPosZ * Math.sin(angleRadian)
+    const newRotatedZ = objFinalPosZ * Math.cos(angleRadian) + objFinalPosX * Math.sin(angleRadian)
 
     return ({ x: newRotatedX, z: newRotatedZ });
   }
---
+++ backend/models/API.js
@@ -0,0 +1,6 @@
+const API = {
+    AviationEdgeURL: "http://aviation-edge.com/v2/public/",
+    OpenSkyURL: "https://opensky-network.org/api/"
+    
+}
+module.exports = API;
